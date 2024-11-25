4f8258840c6b090cc098853bbd387c21c5af7cc9
LorenzLamm <lorenz.lamm@googlemail.com>
2024-11-25 11:18:54 +0100

    pyvista curvature computation

--- src/membrain_stats/protein_concentration/protein_concentration.py
+++ src/membrain_stats/protein_concentration/protein_concentration.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import starfile
 import trimesh
+import scipy
 
 from membrain_stats.utils.io_utils import (
     get_mesh_filenames,
@@ -14,24 +15,59 @@ from membrain_stats.membrane_edges.edge_from_curvature import get_edge_mask
 def protein_concentration_singlemb(
     filename: str,
     pixel_size_multiplier: float = None,
+    pixel_size_multiplier_positions: float = None,
     exclude_edges: bool = False,
     edge_file: str = None,
     edge_exclusion_width: float = 50.0,
     only_one_side: bool = False,
     edge_percentile: float = 95,
+    store_sanity_meshes: bool = False,
 ):
     mesh_dict = get_mesh_from_file(
-        filename, pixel_size_multiplier=pixel_size_multiplier
+        filename,
+        pixel_size_multiplier=pixel_size_multiplier,
+        pixel_size_multiplier_positions=pixel_size_multiplier_positions,
     )
     mesh = trimesh.Trimesh(vertices=mesh_dict["verts"], faces=mesh_dict["faces"])
 
     if exclude_edges:
+        mesh_orig = mesh
         mesh = get_edge_mask(
             mesh=mesh,
             edge_exclusion_width=edge_exclusion_width,
             temp_file=edge_file,
             percentile=edge_percentile,
-        )[0]
+            return_vertex_mask=True,
+        )
+
+        if store_sanity_meshes:
+            # store in file:
+            out_mesh_orig = (
+                "./sanity_meshes/"
+                + os.path.basename(filename).split(".")[0]
+                + "_orig.obj"
+            )
+            out_mesh_cropped = "./sanity_meshes/" + os.path.basename(filename)
+            mesh_orig.export(out_mesh_orig)
+            mesh[0].export(out_mesh_cropped)
+
+        mesh, vertex_mask = mesh
+        positions = mesh_dict["positions"]
+
+        excluded_vertices = mesh_orig.vertices[~vertex_mask]
+        included_vertices = mesh_orig.vertices[vertex_mask]
+
+        # compute nearest neighbor vertex for each position
+        tree_included = scipy.spatial.cKDTree(included_vertices)
+        tree_excluded = scipy.spatial.cKDTree(excluded_vertices)
+
+        nn_distances_inc = tree_included.query(positions, k=1)[0]
+        nn_distances_exc = tree_excluded.query(positions, k=1)[0]
+
+        # exclude vertices that are closer to the excluded vertices than to the included vertices
+        mask = nn_distances_inc < nn_distances_exc
+        positions = positions[mask]
+        mesh_dict["positions"] = positions
 
     area = mesh.area
     area = area * (0.5 if only_one_side else 1.0)
@@ -44,58 +80,6 @@ def protein_concentration_singlemb(
     return num_proteins, area, protein_concentration
 
 
-# def protein_concentration_folder(
-#     in_folder: str,
-#     out_folder: str,
-#     only_one_side: bool = False,
-#     exclude_edges: bool = False,
-#     edge_exclusion_width: float = 50.0,
-#     pixel_size_multiplier: float = None,
-# ):
-
-#     filenames = get_mesh_filenames(in_folder)
-#     mesh_dicts = [get_mesh_from_file(filename) for filename in filenames]
-#     meshes = [
-#         trimesh.Trimesh(
-#             vertices=mesh_dict["verts"]
-#             * (1.0 if pixel_size_multiplier is None else pixel_size_multiplier),
-#             faces=mesh_dict["faces"],
-#         )
-#         for mesh_dict in mesh_dicts
-#     ]
-
-#     if exclude_edges:
-#         out_files_edges = get_tmp_edge_files(out_folder, filenames)
-#         print("Excluding edges. This can take a while.")
-#         meshes = [
-#             get_edge_mask(
-#                 mesh=mesh, edge_exclusion_width=edge_exclusion_width, temp_file=filename
-#             )[0]
-#             for mesh, filename in zip(meshes, out_files_edges)
-#         ]
-
-#     areas = [mesh.area for mesh in meshes]
-#     areas = [area * (0.5 if only_one_side else 1.0) for area in areas]
-
-#     num_proteins = [len(mesh_dict["positions"]) for mesh_dict in mesh_dicts]
-
-#     protein_concentrations = [num_proteins[i] / areas[i] for i in range(len(meshes))]
-#     protein_concentrations = [
-#         concentration * 100 for concentration in protein_concentrations
-#     ]  # get value in nm^-2
-
-#     out_data = {
-#         "filename": filenames,
-#         "num_proteins": num_proteins,
-#         "area": areas,
-#         "protein_concentration": protein_concentrations,
-#     }
-#     out_data = pd.DataFrame(out_data)
-#     out_file = os.path.join(out_folder, "protein_concentration.star")
-#     os.makedirs(out_folder, exist_ok=True)
-#     starfile.write(out_data, out_file)
-
-
 def protein_concentration_folder(
     in_folder: str,
     out_folder: str,
@@ -103,8 +87,10 @@ def protein_concentration_folder(
     exclude_edges: bool = False,
     edge_exclusion_width: float = 50.0,
     pixel_size_multiplier: float = None,
+    pixel_size_multiplier_positions: float = None,
     plot: bool = False,
     edge_percentile: float = 95,
+    store_sanity_meshes: bool = False,
 ):
 
     filenames = get_mesh_filenames(in_folder)
@@ -114,11 +100,13 @@ def protein_concentration_folder(
         protein_concentration_singlemb(
             filename=filename,
             pixel_size_multiplier=pixel_size_multiplier,
+            pixel_size_multiplier_positions=pixel_size_multiplier_positions,
             exclude_edges=exclude_edges,
             edge_file=edge_file,
             edge_exclusion_width=edge_exclusion_width,
             only_one_side=only_one_side,
-            # edge_percentile=edge_percentile,
+            edge_percentile=edge_percentile,
+            store_sanity_meshes=store_sanity_meshes,
         )
         for filename, edge_file in zip(filenames, out_files_edges)
     ]
@@ -135,11 +123,3 @@ def protein_concentration_folder(
     out_file = os.path.join(out_folder, "protein_concentration.star")
     os.makedirs(out_folder, exist_ok=True)
     starfile.write(out_data, out_file)
-
-    # if plot:
-    #     import matplotlib.pyplot as plt
-
-    #     # make histogram
-    #     plt.figure()
-    #     plt.hist(protein_concentrations, bins=10)
-    #     plt.savefig(os.path.join(out_folder, "protein_concentration_histogram.png"))
